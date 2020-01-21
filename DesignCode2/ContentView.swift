a46b5bfd36e1f1022624acbaf65c32b36873642a
Tariq <tmohamma@visa.com>
2020-01-21 12:01:28 +0800

    Initial Commit

---
+++ DesignCode2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignCode2
+//
+//  Created by Tariq Mohammad on 1/21/20.
+//  Copyright © 2020 Tariq Mohammad. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
