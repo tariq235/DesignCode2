e7c3264296c1444b1c573c6548321bb013740bab
Tariq <tmohamma@visa.com>
2020-01-28 09:20:53 +0800

    - Push all code

--- DesignCode2/ContentView.swift
+++ DesignCode2/ContentView.swift
@@ -9,13 +9,170 @@
 import SwiftUI
 
 struct ContentView: View {
+    // animation states
+    @State var show = false
+    @State var viewState = CGSize.zero
+    @State var showCard = false
+
     var body: some View {
-        Text("Hello, World!")
+        // ZStack is useful for putting multiple elements on top of each in a 3 dimensional level (Z)
+        ZStack {
+            TitleView()
+                .blur(radius: show ? 20 : 0)
+                .opacity(showCard ? 0.4 : 1)
+                .offset(y: showCard ? -200 : 0)
+                .animation(
+                    Animation // value type
+                        .default // modifier
+                        .delay(0.1)
+            )
+
+            BackCardView()
+                .frame(width: showCard ? 300 : 340, height: 220)
+                .background(show ? Color("card3") : Color("card4"))
+                .cornerRadius(20)
+                .shadow(radius: 20)
+                .offset(x: 0, y: show ? -400 : -40)
+                .offset(x: viewState.width, y: viewState.height)
+                .offset(y: showCard ? -180 : 0)
+                .scaleEffect(showCard ? 1 : 0.9)
+                .rotationEffect(.degrees(show ? 0 : 10))
+                .rotationEffect(Angle(degrees: showCard ? -10 : 0))
+
+                .rotation3DEffect(Angle(degrees: showCard ? 0 : 10), axis: (x: 10.0, y: 0, z: 0))
+                .blendMode(.hardLight)
+                .animation(.easeInOut(duration: 0.5))
+
+
+            BackCardView()
+                .frame(width: 340, height: 220)
+                .background(show ? Color("card4") : Color("card3"))
+                .cornerRadius(20)
+                .shadow(radius: 20)
+                .offset(x: 0, y: show ? -200 : -20)
+                .offset(x: viewState.width, y: viewState.height)
+                .offset(y: showCard ? -140 : 0)
+                .scaleEffect(showCard ? 1 : 0.95)
+                .rotationEffect(.degrees(show ? 0 : 5))
+                .rotationEffect(Angle(degrees: showCard ? -5 : 0))
+                .rotation3DEffect(Angle(degrees: showCard ? 0 :5), axis: (x: 10.0, y: 0, z: 0))
+                .blendMode(.hardLight)
+                .animation(.easeInOut(duration: 0.3))
+
+            CardView()
+                .frame(width: showCard ? 375 : 340.0, height: 220.0)
+                .background(Color.black)
+//                .cornerRadius(20) // corner radius clip the content
+                .clipShape(RoundedRectangle(cornerRadius: showCard ? 30 : 20, style: .continuous))
+                .shadow(radius: 20)
+                .offset(x: viewState.width, y: viewState.height)
+                .offset(y: showCard ? -100 : 0)
+                .blendMode(.hardLight)
+                .animation(.spring(response: 0.3, dampingFraction: 0.6, blendDuration: 0))
+                .onTapGesture {
+                    self.showCard.toggle()
+            }
+            .gesture(
+                DragGesture().onChanged { value in
+                        self.viewState = value.translation
+                        self.show = true
+                }
+                .onEnded { value in
+                    self.show = false
+                    self.viewState = .zero
+
+                }
+            )
+
+            BottomCardView()
+                .offset(y: showCard ? 360 : 1000)
+                .blur(radius: show ? 20 : 0)
+                .animation(.timingCurve(0.2, 0.8, 0.2, 1, duration: 0.8))
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CardView: View {
+    var body: some View {
+        VStack {
+            // Image next to the VStack will create another HStack
+            // Parent HStack will serve as a container so that we can apply additional styling
+            HStack {
+                // Allow us to stack 2 Text elements together
+                // it'll take care of the layout automatically for more elements
+                VStack(alignment: .leading) {
+                    Text("UI Design")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                    Text("Certificate")
+                        .foregroundColor(Color("accent"))
+                }
+                // By default, SwiftUI stacks will take the minimum size and expand based on content. Using the Spacer will allow you to push everything to a maximum size
+                Spacer()
+                Image("Logo1")
+            }
+            .padding(.top, 20)
+            .padding(.horizontal, 20)
+            Spacer()
+            Image("Card1")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 300, height: 110, alignment: .top)
+        }
+    }
+}
+
+struct BackCardView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+        }
+    }
+}
+
+struct TitleView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Certificates")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                Spacer()
+            }
+            .padding()
+            Image("Background1")
+            Spacer()
+        }
+    }
+}
+
+struct BottomCardView: View {
+    var body: some View {
+        VStack {
+            Rectangle()
+                .frame(width: 40, height: 5)
+                .cornerRadius(3)
+                .opacity(0.1)
+            Text("This certificate is proof that Meng To has achieved the UI Design course with approval from a Design+Code instructor.")
+                .multilineTextAlignment(.center)
+                .font(.subheadline)
+                .lineSpacing(4)
+            Spacer()
+        }
+        .padding(.top, 8)
+        .padding(.horizontal, 20)
+
+            // If you don't always want to use Spacer to use all available space, you can use the Min Max technique. Max width or height is extremely useful for making your layout adaptive.
+            .frame(maxWidth: .infinity)
+            .background(Color.white)
+            .cornerRadius(30)
+            .shadow(radius: 20)
     }
 }
